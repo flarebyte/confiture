03cae19b2cef8aa554909a2702d83b010b97e65a
olih <bitbucket.olivier.4aswabra@flarebyte.com>
2015-05-02 17:33:15 +0100

    Fixed compression issue

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 /*global describe, it */
 'use strict';
 var fs = require('fs-extra');
-var assert = require('assert');
+var assert = require('chai').assert;
 var confiture = require('../');
 
 fs.emptyDirSync(__dirname + "/temp");
@@ -20,13 +20,13 @@ var bad_lodash_json = {
     relativeDirectory: "fixtures"
 };
 
-// var read_gz_lodash_json = {
-//     name: "read-lodash-gz",
-//     compression: "gz",
-//     schema: __dirname + "/fixtures/pack.schema.json",
-//     baseDirectory: __dirname,
-//     relativeDirectory: "fixtures"
-// };
+var read_gz_lodash_json = {
+    name: "read-lodash-gz",
+    compression: "gz",
+    schema: __dirname + "/fixtures/pack.schema.json",
+    baseDirectory: __dirname,
+    relativeDirectory: "fixtures"
+};
 
 var read_aes128_lodash_json = {
     name: "read-lodash-aes",
@@ -70,65 +70,65 @@ var write_aes128_lodash_json = {
 };
 
 var displayError = function(e) {
-    console.error(e);
+    throw new Error("Tests failed with error: " + e);
 };
 
 
 describe('confiture node module', function() {
     it('must configure simple json', function() {
-        var conf = confiture(lodash_json).configuration;
-        assert(conf, 'lodash.json');
+        var conf = confiture(lodash_json).configuration();
+        assert.equal(conf.name, "lodash");
     });
 
     it('must load and validate simple json', function() {
         var json = confiture(lodash_json).load();
-        assert(json.name, 'lodash');
+        assert.equal(json.name, 'lodash');
+        assert.equal(json.version, '3.7.0');
     });
 
     it('must not load and validate bad json', function() {
-        try {
-            confiture(bad_lodash_json).load();
-        } catch (e) {
-            assert(e != null, 'lodash');
-        }
+        assert.instanceOf(confiture(bad_lodash_json).load(), Error, "should detect misconfiguration");
     });
-    // it('must load and validate compressed json', function() {
-    //     var json = confiture(read_gz_lodash_json).load();
-    //     assert(json.name, 'lodash');
-    // });
-    
+
+    it('must load and validate compressed json', function() {
+        var json = confiture(read_gz_lodash_json).load();
+        assert.equal(json.name, 'lodash');
+        assert.equal(json.version, '3.7.0');
+    });
+
     it('must load and validate encryped json', function() {
         var json = confiture(read_aes128_lodash_json).load();
-        assert(json.name, 'lodash');
+        assert.equal(json.name, 'lodash');
+        assert.equal(json.version, '3.7.0');
     });
 
     it('must save and validate simple json', function() {
         var json = confiture(lodash_json).load();
         var stream = confiture(write_lodash_json).save(json);
         stream.on("error", displayError);
-        assert(stream != null, 'lodash');
+        assert.isNotNull(stream);
     });
 
     it('must save and validate simple json with backup', function() {
         var json = confiture(lodash_json).load();
-        fs.writeJsonSync(__dirname+"/temp/write-lodash-bak.json",json);
+        fs.writeJsonSync(__dirname + "/temp/write-lodash-bak.json", json);
         var stream = confiture(write_lodash_json_with_backup).save(json);
         stream.on("error", displayError);
-        assert(stream != null, 'lodash');
+        assert.isNotNull(stream);
     });
 
     it('must validate, compress, save json', function() {
         var json = confiture(lodash_json).load();
         var stream = confiture(write_gz_lodash_json).save(json);
         stream.on("error", displayError);
-        assert(stream != null, 'lodash');
+        assert.isNotNull(stream);
     });
 
     it('must validate, encrypt, save json', function() {
         var json = confiture(lodash_json).load();
         var stream = confiture(write_aes128_lodash_json).save(json);
         stream.on("error", displayError);
-        assert(stream != null, 'lodash');
+        assert.isNotNull(stream);
     });
 
 
