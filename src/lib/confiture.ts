249ed1d2d19fc0fc4e8c6b7431dce89b5683954f
olih <olih@users.noreply.github.com>
2019-10-27 13:15:00 +0000

    linting refactoring

---
+++ src/lib/confiture.ts
@@ -0,0 +1,309 @@
+'use strict';
+import fs from 'fs-extra';
+import stream from 'stream';
+import zlib from 'zlib';
+const Readable = stream.Readable;
+import Joi from '@hapi/joi';
+import crypto from 'crypto';
+import validator from 'is-my-json-valid';
+import _ from 'lodash';
+import moment from 'moment';
+import path from 'path';
+import util from 'util';
+
+const SPACES = 4;
+const OK = 'OK';
+
+const confSchema = Joi.object()
+  .keys({
+    name: Joi.string()
+      .min(2)
+      .required()
+      .description('base name for the configuration file'),
+    compression: Joi.string()
+      .valid('gz')
+      .optional()
+      .description('compression algorithm')
+      .example('gz'),
+    encryption: Joi.string()
+      .valid(crypto.getCiphers())
+      .optional()
+      .description('encryption algorithm')
+      .example('aes-256-cbc'),
+    password: Joi.string()
+      .min(2)
+      .description('password for encryption'),
+    schema: [
+      Joi.string()
+        .min(2)
+        .description('json-schema file path'),
+      Joi.object().description('json-schema content')
+    ],
+    baseDirectory: Joi.string()
+      .min(2)
+      .required()
+      .description('parent directory containing the configuration file'),
+    relativeDirectory: Joi.string()
+      .min(2)
+      .optional()
+      .description('the child directory containing the configuration file'),
+    backupBeforeSave: Joi.boolean()
+      .default(false)
+      .description('true if the configuration file must be backup before save')
+  })
+  .with('encryption', ['password']);
+
+const isSchemaFile = (value: any) => {
+  return _.isString(value);
+};
+
+const isSchemaContent = (value: any) => {
+  return _.isPlainObject(value);
+};
+
+const loadSchema = (value: any) => {
+  if (isSchemaContent(value)) {
+    return value;
+  } else if (isSchemaFile(value)) {
+    return fs.readJsonSync(value);
+  } else {
+    throw new Error('Unknown schema ' + value);
+  }
+};
+
+const schemaValidator = (value: any) => {
+  const schema = loadSchema(value);
+  return validator(schema);
+};
+
+interface Confiture {
+  configuration: object;
+  load: any;
+  save(conf: object): object;
+  saveSync(conf: object): string | Error;
+}
+export function confiture(config: object): Confiture {
+  const confg = confSchema.validate(config);
+
+  if (!_.isNull(confg.error)) {
+    throw new Error(confg.error.message);
+  }
+
+  const cfg = confg.value;
+
+  const isCompressed = _.has(cfg, 'compression');
+  const isEncrypted = _.has(cfg, 'encryption');
+  const hasRelativeDirectory = _.has(cfg, 'relativeDirectory');
+
+  const validate = schemaValidator(cfg.schema); // sync
+
+  let content = null;
+
+  const getConfigurationFilename = () => {
+    let filename = cfg.name + '.json';
+    if (isEncrypted) {
+      filename = filename + '.' + cfg.encryption;
+    } else if (isCompressed) {
+      filename = filename + '.' + cfg.compression;
+    }
+
+    return filename;
+  };
+
+  const confFilename = getConfigurationFilename();
+
+  cfg.filename = confFilename;
+
+  const filepath = hasRelativeDirectory
+    ? path.join(cfg.baseDirectory, cfg.relativeDirectory, confFilename)
+    : path.join(cfg.baseDirectory, confFilename);
+
+  cfg.filepath = filepath;
+
+  const backup = () => {
+    const suffix = moment().format();
+    const backupFilepath = filepath + '.bak-' + suffix;
+    try {
+      fs.renameSync(filepath, backupFilepath);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const loadJson = () => {
+    const jsonToCheck = fs.readJsonSync(filepath);
+    return jsonToCheck;
+  };
+
+  const loadCompressedJson = () => {
+    const compressed = fs.readFileSync(filepath);
+    const uncompressed = zlib.gunzipSync(compressed).toString();
+    const jsonToCheck = JSON.parse(uncompressed);
+    return jsonToCheck;
+  };
+
+  const loadEncryptedJson = () => {
+    const encrypted = fs.readFileSync(filepath);
+    const decipher = crypto.createDecipher(cfg.encryption, cfg.password);
+    let dec = decipher.update(encrypted, undefined, 'utf8');
+    dec += decipher.final('utf8');
+    const jsonToCheck = JSON.parse(dec);
+    return jsonToCheck;
+  };
+
+  const load = () => {
+    let loaded = null;
+    if (isEncrypted) {
+      loaded = loadEncryptedJson();
+    } else if (isCompressed) {
+      loaded = loadCompressedJson();
+    } else {
+      loaded = loadJson();
+    }
+
+    if (!validate(loaded)) {
+      return new Error(
+        util.format('Failed validation while loading: %j', validate.errors)
+      );
+    }
+
+    content = loaded;
+    return content;
+  };
+
+  const saveJson = (wishedJson: object) => {
+    const jsonStr = JSON.stringify(wishedJson, null, SPACES);
+    const wstream = fs.createWriteStream(filepath);
+    wstream.write(jsonStr);
+    return wstream;
+  };
+
+  const saveJsonSync = (wishedJson: object) => {
+    try {
+      fs.writeJsonSync(filepath, wishedJson, { spaces: SPACES });
+      return OK;
+    } catch (err) {
+      return err;
+    }
+  };
+
+  const stringReader = (str: string) => {
+    const r = new Readable();
+    r._read = () => {
+      r.push(str);
+      r.push(null);
+    };
+    return r;
+  };
+
+  const saveCompressedJson = (wishedJson: object) => {
+    const jsonStr = JSON.stringify(wishedJson, null, SPACES);
+    const rstream = stringReader(jsonStr);
+    const zipstream = cfg === 'gz' ? zlib.createGzip() : zlib.createGzip(); // TODO (zip?)
+    const wstream = fs.createWriteStream(filepath);
+    rstream.pipe(zipstream).pipe(wstream);
+    return wstream;
+  };
+
+  const saveCompressedJsonSync = (wishedJson: object)=> {
+    const jsonStr = JSON.stringify(wishedJson, null, SPACES);
+    try {
+      const compressed = zlib.gzipSync(jsonStr);
+      fs.writeFileSync(filepath, compressed);
+      return OK;
+    } catch (err) {
+      return err;
+    }
+  };
+
+  const saveEncrypedJson = (wishedJson: object) => {
+    const jsonStr = JSON.stringify(wishedJson, null, SPACES);
+    const rstream = stringReader(jsonStr);
+    const encryptor = crypto.createCipher(cfg.encryption, cfg.password);
+    const wstream = fs.createWriteStream(filepath);
+    rstream.pipe(encryptor).pipe(wstream);
+    return wstream;
+  };
+
+  const saveEncrypedJsonSync = (wishedJson: object) => {
+    const jsonStr = JSON.stringify(wishedJson, null, SPACES);
+    const encryptor = crypto.createCipher(cfg.encryption, cfg.password);
+    try {
+      let enc = encryptor.update(jsonStr, 'utf8', 'hex');
+      enc += encryptor.final('hex');
+      const buff = new Buffer(enc, 'hex');
+      fs.writeFileSync(filepath, buff);
+      return OK;
+    } catch (err) {
+      return err;
+    }
+  };
+
+  const confDirectory = hasRelativeDirectory
+    ? path.join(cfg.baseDirectory, cfg.relativeDirectory)
+    : cfg.baseDirectory;
+  const ensureDirectory = () => {
+    fs.ensureDir(confDirectory);
+  };
+
+  const save = (wishedJson: object) =>{
+    if (!validate(wishedJson)) {
+      return new Error(
+        util.format('Failed validation while saving: %j', validate.errors)
+      );
+    }
+    ensureDirectory();
+
+    if (cfg.backupBeforeSave) {
+      backup();
+    }
+    content = wishedJson;
+    let rstream = null;
+
+    if (isEncrypted) {
+      rstream = saveEncrypedJson(wishedJson);
+    } else if (isCompressed) {
+      rstream = saveCompressedJson(wishedJson);
+    } else {
+      rstream = saveJson(wishedJson);
+    }
+
+    return rstream;
+  };
+
+  const saveSync = (wishedJson: object) => {
+    if (!validate(wishedJson)) {
+      return new Error(
+        util.format('Failed validation while saving: %j', validate.errors)
+      );
+    }
+    ensureDirectory();
+
+    if (cfg.backupBeforeSave) {
+      backup();
+    }
+    content = wishedJson;
+    let saveResult = OK;
+
+    if (isEncrypted) {
+      saveResult = saveEncrypedJsonSync(wishedJson);
+    } else if (isCompressed) {
+      saveResult = saveCompressedJsonSync(wishedJson);
+    } else {
+      saveResult = saveJsonSync(wishedJson);
+    }
+
+    return saveResult;
+  };
+
+  const getConfiguration = () => {
+    return _.clone(cfg);
+  };
+
+  return {
+    configuration: getConfiguration,
+    load,
+    save,
+    saveSync
+  };
+}
--- src/test.spec.ts
+++ src/test.spec.ts
@@ -1,25 +1,25 @@
 /*global describe, it */
 'use strict';
-import  fs from 'fs-extra';
-import confiture from '.';
+import fs from 'fs-extra';
+import confiture from './lib/confiture';
 
 fs.emptyDirSync(__dirname + '/temp');
 
-let lodash_json = {
+const lodash_json = {
   name: 'lodash',
   schema: __dirname + '/fixtures/pack.schema.json',
   baseDirectory: __dirname,
   relativeDirectory: 'fixtures'
 };
 
-let bad_lodash_json = {
+const bad_lodash_json = {
   name: 'bad-lodash',
   schema: __dirname + '/fixtures/pack.schema.json',
   baseDirectory: __dirname,
   relativeDirectory: 'fixtures'
 };
 
-let read_gz_lodash_json = {
+const read_gz_lodash_json = {
   name: 'read-lodash-gz',
   compression: 'gz',
   schema: __dirname + '/fixtures/pack.schema.json',
@@ -27,7 +27,7 @@ let read_gz_lodash_json = {
   relativeDirectory: 'fixtures'
 };
 
-let read_aes128_lodash_json = {
+const read_aes128_lodash_json = {
   name: 'read-lodash-aes',
   encryption: 'aes-256-cbc',
   password: 'confiture rocks',
@@ -36,21 +36,21 @@ let read_aes128_lodash_json = {
   relativeDirectory: 'fixtures'
 };
 
-let write_lodash_json = {
+const write_lodash_json = {
   name: 'write-lodash',
   schema: __dirname + '/fixtures/pack.schema.json',
   baseDirectory: __dirname,
   relativeDirectory: 'temp'
 };
 
-let write_sync_lodash_json = {
+const write_sync_lodash_json = {
   name: 'write-sync-lodash',
   schema: __dirname + '/fixtures/pack.schema.json',
   baseDirectory: __dirname,
   relativeDirectory: 'temp'
 };
 
-let write_lodash_json_with_backup = {
+const write_lodash_json_with_backup = {
   name: 'write-lodash-bak',
   backupBeforeSave: true,
   schema: __dirname + '/fixtures/pack.schema.json',
@@ -58,7 +58,7 @@ let write_lodash_json_with_backup = {
   relativeDirectory: 'temp'
 };
 
-let write_sync_lodash_json_with_backup = {
+const write_sync_lodash_json_with_backup = {
   name: 'write-sync-lodash-bak',
   backupBeforeSave: true,
   schema: __dirname + '/fixtures/pack.schema.json',
@@ -66,7 +66,7 @@ let write_sync_lodash_json_with_backup = {
   relativeDirectory: 'temp'
 };
 
-let write_gz_lodash_json = {
+const write_gz_lodash_json = {
   name: 'write-lodash',
   compression: 'gz',
   schema: __dirname + '/fixtures/pack.schema.json',
@@ -74,7 +74,7 @@ let write_gz_lodash_json = {
   relativeDirectory: 'temp'
 };
 
-let write_sync_gz_lodash_json = {
+const write_sync_gz_lodash_json = {
   name: 'write-sync-lodash',
   compression: 'gz',
   schema: __dirname + '/fixtures/pack.schema.json',
@@ -82,7 +82,7 @@ let write_sync_gz_lodash_json = {
   relativeDirectory: 'temp'
 };
 
-let write_aes128_lodash_json = {
+const write_aes128_lodash_json = {
   name: 'write-lodash',
   encryption: 'aes-256-cbc',
   password: 'confiture rocks',
@@ -91,7 +91,7 @@ let write_aes128_lodash_json = {
   relativeDirectory: 'temp'
 };
 
-let write_sync_aes128_lodash_json = {
+const write_sync_aes128_lodash_json = {
   name: 'write-sync-lodash',
   encryption: 'aes-256-cbc',
   password: 'confiture rocks',
@@ -100,18 +100,18 @@ let write_sync_aes128_lodash_json = {
   relativeDirectory: 'temp'
 };
 
-let displayError = function(e) {
+const displayError = function(e) {
   throw new Error('Tests failed with error: ' + e);
 };
 
 describe('confiture node module', function() {
   it('must configure simple json', function() {
-    let conf = confiture(lodash_json).configuration();
+    const conf = confiture(lodash_json).configuration();
     assert.equal(conf.name, 'lodash');
   });
 
   it('must load and validate simple json', function() {
-    let json = confiture(lodash_json).load();
+    const json = confiture(lodash_json).load();
     assert.equal(json.name, 'lodash');
     assert.equal(json.version, '3.7.0');
   });
@@ -125,68 +125,68 @@ describe('confiture node module', function() {
   });
 
   it('must load and validate compressed json', function() {
-    let json = confiture(read_gz_lodash_json).load();
+    const json = confiture(read_gz_lodash_json).load();
     assert.equal(json.name, 'lodash');
     assert.equal(json.version, '3.7.0');
   });
 
   it('must load and validate encryped json', function() {
-    let json = confiture(read_aes128_lodash_json).load();
+    const json = confiture(read_aes128_lodash_json).load();
     assert.equal(json.name, 'lodash');
     assert.equal(json.version, '3.7.0');
   });
 
   it('must save and validate simple json', function() {
-    let json = confiture(lodash_json).load();
-    let stream = confiture(write_lodash_json).save(json);
+    const json = confiture(lodash_json).load();
+    const stream = confiture(write_lodash_json).save(json);
     stream.on('error', displayError);
     assert.isNotNull(stream);
   });
 
   it('must save and validate simple json synchronously', function() {
-    let json = confiture(lodash_json).load();
-    let result = confiture(write_sync_lodash_json).saveSync(json);
+    const json = confiture(lodash_json).load();
+    const result = confiture(write_sync_lodash_json).saveSync(json);
     assert.equal(result, 'OK');
   });
 
   it('must save and validate simple json with backup', function() {
-    let json = confiture(lodash_json).load();
+    const json = confiture(lodash_json).load();
     fs.writeJsonSync(__dirname + '/temp/write-lodash-bak.json', json);
-    let stream = confiture(write_lodash_json_with_backup).save(json);
+    const stream = confiture(write_lodash_json_with_backup).save(json);
     stream.on('error', displayError);
     assert.isNotNull(stream);
   });
 
   it('must save and validate simple json with backup synchronously', function() {
-    let json = confiture(lodash_json).load();
+    const json = confiture(lodash_json).load();
     fs.writeJsonSync(__dirname + '/temp/write-sync-lodash-bak.json', json);
-    let result = confiture(write_sync_lodash_json_with_backup).saveSync(json);
+    const result = confiture(write_sync_lodash_json_with_backup).saveSync(json);
     assert.equal(result, 'OK');
   });
 
   it('must validate, compress, save json', function() {
-    let json = confiture(lodash_json).load();
-    let stream = confiture(write_gz_lodash_json).save(json);
+    const json = confiture(lodash_json).load();
+    const stream = confiture(write_gz_lodash_json).save(json);
     stream.on('error', displayError);
     assert.isNotNull(stream);
   });
 
   it('must validate, compress, save json synchronously', function() {
-    let json = confiture(lodash_json).load();
-    let result = confiture(write_sync_gz_lodash_json).saveSync(json);
+    const json = confiture(lodash_json).load();
+    const result = confiture(write_sync_gz_lodash_json).saveSync(json);
     assert.equal(result, 'OK');
   });
 
   it('must validate, encrypt, save json', function() {
-    let json = confiture(lodash_json).load();
-    let stream = confiture(write_aes128_lodash_json).save(json);
+    const json = confiture(lodash_json).load();
+    const stream = confiture(write_aes128_lodash_json).save(json);
     stream.on('error', displayError);
     assert.isNotNull(stream);
   });
 
   it('must validate, encrypt, save json synchronously', function() {
-    let json = confiture(lodash_json).load();
-    let result = confiture(write_sync_aes128_lodash_json).saveSync(json);
+    const json = confiture(lodash_json).load();
+    const result = confiture(write_sync_aes128_lodash_json).saveSync(json);
     assert.equal(result, 'OK');
   });
 });
